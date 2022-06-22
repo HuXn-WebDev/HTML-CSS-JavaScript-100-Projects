f44c633321229d82bd8b2394789126740661e11f
HuXn WebDev <webdevacademy6@gmail.com>
2022-06-22 14:53:20 -0700

    Auto Text

---
+++ 16. Auto Text/app.js
@@ -0,0 +1,39 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+
+const words = ["Awesome", "Fun", "Cool", "Life", "Famous", "Weird"];
+const typingDelay = 200;
+const erasingDelay = 200;
+const newLetterDelay = 2000;
+let index = 0;
+let charIndex = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (words.length) {
+    setTimeout(type, newLetterDelay);
+  }
+});
+
+function type() {
+  if (charIndex < words[index].length) {
+    typedTextSpan.textContent += words[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newLetterDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = words[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    index++;
+    if (index >= words.length) {
+      index = 0;
+    }
+    setTimeout(type, typingDelay + 1100);
+  }
+}
