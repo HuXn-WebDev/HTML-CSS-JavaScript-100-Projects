000327ee26398616f93565f0f68187bc0710cdb4
HuXn WebDev <webdevacademy6@gmail.com>
2022-07-18 20:46:09 -0700

    JavaScript Accordions Project

---
+++ 40. Accordions/app.js
@@ -0,0 +1,22 @@
+const itemHeaders = document.querySelectorAll(".accordion-item-header");
+
+itemHeaders.forEach((accordion) => {
+  accordion.addEventListener("click", collapseAccordions);
+
+  function collapseAccordions() {
+    const activeAccordion = document.querySelector(".active");
+    if (activeAccordion && activeAccordion !== accordion) {
+      activeAccordion.classList.toggle("active");
+      activeAccordion.nextElementSibling.style.maxHeight = 0;
+    }
+
+    accordion.classList.toggle("active");
+    const accordionItemBody = accordion.nextElementSibling;
+
+    if (accordion.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+  }
+});
