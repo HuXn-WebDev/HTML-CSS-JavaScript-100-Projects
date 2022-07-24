2bee6072f6ef9c9b06599ec7fdcdd08bf2548fe1
HuXn WebDev <webdevacademy6@gmail.com>
2022-07-24 12:57:30 -0700

    Digital Clock

---
+++ 45. Digital Clock/app.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector(".clock");
+
+clock.addEventListener("load", tick);
+
+function tick() {
+  const now = new Date();
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+
+  const html = `
+        <span>${h} :</span>
+        <span>${m} :</span>
+        <span>${s}</span>
+    `;
+  clock.innerHTML = html;
+}
+
+setInterval(tick, 1000);
